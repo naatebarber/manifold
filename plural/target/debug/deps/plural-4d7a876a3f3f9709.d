67a7fdaad35e1bb51619a9e8db8ac0adc8b2710c
Nathan Barber <nathanbarber@o11.lan>
2024-03-31 18:22:50 -0400

    Initial commit

---
+++ plural/target/debug/deps/plural-4d7a876a3f3f9709.d
@@ -0,0 +1,5 @@
+/Users/nathanbarber/Desktop/plural/plural/target/debug/deps/libplural-4d7a876a3f3f9709.rmeta: src/lib.rs
+
+/Users/nathanbarber/Desktop/plural/plural/target/debug/deps/plural-4d7a876a3f3f9709.d: src/lib.rs
+
+src/lib.rs:
---
+++ plural/target/debug/deps/plural-7e3e00dbabc112f1.d
@@ -0,0 +1,5 @@
+/Users/nathanbarber/Desktop/plural/plural/target/debug/deps/libplural-7e3e00dbabc112f1.rmeta: src/lib.rs
+
+/Users/nathanbarber/Desktop/plural/plural/target/debug/deps/plural-7e3e00dbabc112f1.d: src/lib.rs
+
+src/lib.rs:
